27484e48f1d454dba102ca735b8d9aaa8ad949ad
泽泽兮 <yandiwu97@163.com>
2020-11-25 14:47:01 +0800

    初始化

---
+++ src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    河马
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'indexView',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
